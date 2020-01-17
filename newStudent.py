c7f75fd0f5e268e668bbe017c5d0a564135a36d8
Atharva Adhav <55144069+atharva-adhav@users.noreply.github.com>
2020-01-17 23:55:47 +0530

    Add files via upload

---
+++ newStudent.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class NewStudentRegister(FlaskForm):
+    studentName = StringField('studentName', validators=[DataRequired(), Length(max=50)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    confirmPassword = PasswordField('ConfirmPassword', validators=[DataRequired(), EqualTo('password')])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    university = StringField('University', validators=[DataRequired(), Length(max=50)])
+    college = StringField('College', validators=[DataRequired(), Length(max=50)])
+    GrNo = StringField('GrNo', validators=[DataRequired()])
+    MobileNo = StringField('MobileNo', validators=[DataRequired(), Length(min=10, max=10)])
+    submit = SubmitField('Submit')
