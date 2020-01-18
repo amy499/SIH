225559be5fca1bb098e6e74bd54c66d66608f847
Asmi06 <47266957+Asmi06@users.noreply.github.com>
2020-01-18 11:26:09 +0530

    Add files via upload

---
+++ Components/Navigation/Navigation.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+
+class Navigation extends React.Component {
+  render() {
+    return (
+      <Navbar fixed="sticky" bg="dark" expand="xxl" variant="dark">
+        <Navbar.Brand href="#">Student Grievance System</Navbar.Brand>
+
+        <Nav.Link href="#" className="ml-auto">
+          Home
+        </Nav.Link>
+        <Nav.Link href="#">Profile</Nav.Link>
+        <Nav.Link href="#">Notification</Nav.Link>
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto justify-content-end ">
+            <Nav.Link href="#">About</Nav.Link>
+            <Nav.Link href="#">FAQs</Nav.Link>
+            <Nav.Link href="#">Reference Complaints</Nav.Link>
+            <Nav.Link href="#">Log out</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default Navigation;
