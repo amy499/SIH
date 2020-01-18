2f63960154342936b4462c51b75edcd2ee218808
amy499 <45054797+amy499@users.noreply.github.com>
2020-01-18 11:22:16 +0530

    Add files via upload

---
+++ components/PublicGrievance/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PublicGrievanceList from "./public";
+import listData from "./listData";
+
+class PublicGrievance extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      listsData: listData
+    };
+  }
+  render() {
+    const list = this.state.listsData.map(lists => (
+      <PublicGrievanceList
+        key={lists.id}
+        subject={lists.subject}
+        signedby={lists.signedby}
+        startdate={lists.startdate}
+        complaintcontent={lists.complaintcontent}
+        solution={lists.solution}
+      />
+    ));
+
+    return (
+      <div>{list}</div>
+      // <PetitionModal />
+    );
+  }
+}
+
+export default PublicGrievance;
