56f16c369e86dfbd022985b09bdd5c7112b209ae
amy499 <45054797+amy499@users.noreply.github.com>
2020-01-19 11:06:37 +0530

    Update App.js

--- components/InfoBar/App.js
+++ components/InfoBar/App.js
@@ -1,34 +1,41 @@
 import React from "react"
+import style from "./InfoBar.css";
 
-let style={
-  position: 'relative',
-  width: '100%',
-  height: '129px',
-  left: '0px',
-  backgroundColor: '#a8e3ff',
-  color: '#1a1a1a',
-  fontFamily: 'Roboto',
-  textAlign:'center',
-  fontSize: '26px',
-}
+// let style={
+//   position: 'relative',
+//   width: '100%',
+//   height: '129px',
+//   left: '0px',
+//   backgroundColor: '#a8e3ff',
+//   color: '#1a1a1a',
+//   fontFamily: 'Roboto',
+//   textAlign:'center',
+//   fontSize: '26px',
+// }
 
-let styleh2={
-  padding:'42px',
-  backgroundColor: "#e6ccff"
-}
+// let styleh2={
+//   padding:'42px',
+//   backgroundColor: "#e6ccff"
+// }
 
 function InfoBar (props){
   if(props.user.type=='student'){
     return(
-      <div className="InfoBar" style={style}>
-      <h2 style={styleh2}>Welcome {props.user.type}</h2>
+//       <div className="InfoBar" style={style}>
+//       <h2 style={styleh2}>Welcome {props.user.type}</h2>
+//       </div>
+      <div className="infobar">
+        <h2 className="bartitle">Welcome {props.user.type}</h2>
       </div>
     )
   }
   else {
     return(
-      <div>
-      <h2>Committee Name</h2>
+//       <div>
+//       <h2>Committee Name</h2>
+//       </div>
+      <div className="infobar">
+        <h2 className="bartitle">Committee Name</h2>
       </div>
     )
   }
